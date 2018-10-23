4fcf27dcb7edd666c8f5d7efd0a97711961ad919
Björn Ritzl <bjorn.ritzl@gmail.com>
2018-10-23 18:06:11 +0200

    Changed from old platformer code to platypus. Additional cleanup.

--- game/slime.go
+++ game/slime.go
@@ -27,9 +27,9 @@ embedded_components {
   "mask: \"activator\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: 0.0\n"
+  "      x: -11.0\n"
   "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -40,11 +40,26 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 3\n"
+  "    count: 1\n"
   "  }\n"
-  "  data: 25.0\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      x: 11.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 1\n"
+  "    count: 1\n"
+  "  }\n"
   "  data: 14.0\n"
-  "  data: 10.0\n"
+  "  data: 14.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
@@ -72,7 +87,7 @@ embedded_components {
   ""
   position {
     x: 0.0
-    y: 0.0
+    y: -1.0
     z: 0.0
   }
   rotation {
